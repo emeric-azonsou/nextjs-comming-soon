bec39b0153d7a1e424d28b050887b6102b91f897
EmericAz <azonsou158@gmail.com>
2025-03-24 23:49:32 +0000

    .

--- data/data.ts
+++ data/data.ts
@@ -1,10 +1,10 @@
 const data = {
-  sitename: "Comming Soon Template",
-  sitetagline: "Comming Soon Template Tagline",
+  sitename: "Bientôt disponible ",
+  sitetagline: "Bientôt disponible ",
   sitelogo: "",
-  title: "Comming Soon",
+  title: "Bientôt disponible ",
   description:
-    "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+    "Nous travaillons actuellement 👨‍💻 d'arrache-pied pour vous apporter quelque chose de génial, et nous avons hâte de le partager avec vous. Notre équipe met la touche finale à un nouveau projet 🚀 que nous pensons que vous allez adorer.",
   newsletterheading: "Stay tuned for our live updates!",
   copyrightText: "Copyright © 2024 | Roc Creative Studio",
   socialIconsHeading: "Follow Us 📣",
